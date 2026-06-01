813175938907f26c18d6493269cb52d51617ccd7
Everton Oliveira <everton.bps@gmail.com>
2026-06-01 18:41:27 -0300

    feat: hash user password with bcrypt before saving to database

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,7 @@
 // 1. Importamos o nosso Repositório
 // Atenção de Tech Lead: No padrão moderno ESM, importações locais precisam do ".js" no final, mesmo o arquivo sendo ".ts".
 import { UserRepository } from '../repositories/user.repository.js';
+import bcrypt from 'bcrypt';
 
 export class UserService {
   // Instanciamos o repositório para podermos usá-lo dentro desta classe
@@ -8,16 +9,18 @@ export class UserService {
 
   // A função "execute" vai receber o pedido e fazer as validações
   async execute(name: string, email: string, password: string) {
-    
+
     // Regra de Negócio 1: Validação básica
     if (!name || !email || !password) {
-        throw new Error("Todos os campos (nome, email e senha) são obrigatórios.");
+      throw new Error("Todos os campos (nome, email e senha) são obrigatórios.");
     }
 
-    // TODO: No futuro, vamos adicionar a criptografia da senha e verificar e-mail duplicado bem aqui!
+    // Regra de Negócio 2: Criptografia
+    // O número 10 é o "salt", a força do embaralhamento. É o padrão seguro da indústria.
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Se passou por todas as regras, mandamos o Repositório salvar no banco
-    const user = await this.userRepository.create(name, email, password);
+    // Se passou por todas as regras, mandamos o Repositório salvar no banco (usando a senha criptografada!)
+    const user = await this.userRepository.create(name, email, hashedPassword);
 
     return user;
   }
